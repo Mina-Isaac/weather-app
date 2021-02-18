d7760bfb34ef719277072f9e89dc974cc6fc37b9
Mina Isaav <mina.isaac.2000@gmail.com>
2021-02-18 22:09:05 +0200

    Updates

---
+++ precache-manifest.2b904cf603bd4167447e8a151f2018a0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72c10e10b952333faa0114138ec02afa",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "905cef57a272d15784e1",
+    "url": "/weather-app/static/css/2.24767caf.chunk.css"
+  },
+  {
+    "revision": "107341f2540271b101cb",
+    "url": "/weather-app/static/css/main.8b0dc7c4.chunk.css"
+  },
+  {
+    "revision": "905cef57a272d15784e1",
+    "url": "/weather-app/static/js/2.ea09e2b5.chunk.js"
+  },
+  {
+    "revision": "107341f2540271b101cb",
+    "url": "/weather-app/static/js/main.bb880631.chunk.js"
+  },
+  {
+    "revision": "20b7fe9aece0c845f972",
+    "url": "/weather-app/static/js/runtime-main.630a9fd3.js"
+  },
+  {
+    "revision": "6bb8b08b3660bb1861729417d3616f5e",
+    "url": "/weather-app/static/media/background.6bb8b08b.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/weather-app/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/weather-app/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/weather-app/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/weather-app/static/media/slick.f97e3bbf.svg"
+  }
+]);
