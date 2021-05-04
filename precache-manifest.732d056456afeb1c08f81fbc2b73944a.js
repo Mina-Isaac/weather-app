42764e61c3860f1076bf2c647df92d2b0d2fd3d3
Mina Isaac <isaac@int.appeya.com>
2021-05-04 12:41:56 +0200

    Updates

--- precache-manifest.732d056456afeb1c08f81fbc2b73944a.js
+++ precache-manifest.732d056456afeb1c08f81fbc2b73944a.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3f735d1c969ba3015d6e80755d5b4b4a",
+    "revision": "cd11f6541ce7fa3cbcefe85364e7eb3b",
     "url": "/weather-app/index.html"
   },
   {
-    "revision": "905cef57a272d15784e1",
+    "revision": "c5386a58b06823189a80",
     "url": "/weather-app/static/css/2.24767caf.chunk.css"
   },
   {
-    "revision": "acaf6d3ecf7671d0d3a2",
+    "revision": "7104fa654b043c6bb1bb",
     "url": "/weather-app/static/css/main.8b0dc7c4.chunk.css"
   },
   {
-    "revision": "905cef57a272d15784e1",
-    "url": "/weather-app/static/js/2.ea09e2b5.chunk.js"
+    "revision": "c5386a58b06823189a80",
+    "url": "/weather-app/static/js/2.31e61305.chunk.js"
   },
   {
-    "revision": "acaf6d3ecf7671d0d3a2",
-    "url": "/weather-app/static/js/main.3177236c.chunk.js"
+    "revision": "7104fa654b043c6bb1bb",
+    "url": "/weather-app/static/js/main.9d4d976c.chunk.js"
   },
   {
     "revision": "20b7fe9aece0c845f972",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/weather-app/precache-manifest.c2da46b90728fa02ba0381170d9bfb3b.js"
+  "/weather-app/precache-manifest.732d056456afeb1c08f81fbc2b73944a.js"
 );
 
 self.addEventListener('message', (event) => {
